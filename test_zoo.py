e6ec0f366002b8281dd3da6f55ffff988038717d
KKKKHXX <119933864+kkkkhxx@users.noreply.github.com>
2024-11-11 05:03:07 +0000

    6687063

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,33 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
+    def test_child0_12_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_child0_12_0ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+
+    # Add your additional test cases here.
+    def test_teen13_20_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+
+    def test_teen13_20_20ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    # Add your additional test cases here.
+    def test_ad21_60_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    
+    def test_ad21_60_21ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    # Add your additional test cases here.
+    def test_ad_60up_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
+    # Add your additional test cases here.
+    def test_inv_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Invalid")
 
 
 if __name__ == '__main__':
